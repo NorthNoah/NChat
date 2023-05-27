b6dcc32ed1fc24513467e981c15c6becb260b09c
NorthNoah <1264918006@qq.com>
2023-05-27 21:07:41 +0800

    feat：后端server连接数据库

--- NChat-Server/index.js
+++ NChat-Server/index.js
@@ -1,17 +1,36 @@
+const PORT=6000
+const MONGO_URL="mongodb://localhost:27017/chat"
 const express = require("express")
 const app = express()
-const PORT = 6000
 
-// 导入HTTP和cors库
-const server = require("http").Server(app)
+// 导入cors库
 const cors = require("cors")
-
+const mongoose = require("mongoose")
 app.use(cors())
+app.use(express.json())
+
+// mongoose连接数据库
+mongoose
+	.connect(MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("DB Connetion Successfull")
+	})
+	.catch((err) => {
+		console.log(err.message)
+	})
+
+// 监听对应端口
+const server = app.listen(PORT, () =>
+	console.log(`Server started on ${PORT}`)
+)
 
 // 导入socket.io库
-const { Server } = require("socket.io")
+const socket = require("socket.io")
 // 创建实时连接
-const socketIO = new Server(server, {
+const socketIO = socket(server, {
 	cors: {
 		origin: "http://localhost:3000",
 	},
@@ -27,8 +46,4 @@ app.get("/api", (req, res) => {
 	res.json({
 		message: "Hello World",
 	})
-})
-
-app.listen(PORT, () => {
-	console.log(`server listening on ${PORT}`)
 })

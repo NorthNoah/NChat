768c915a29d70df6d73440de6732e718d7fc0b46
NorthNoah <1264918006@qq.com>
2023-05-29 20:23:19 +0800

    feat：后端设置头像路由

--- NChat-Server/routes/userRoutes.js
+++ NChat-Server/routes/userRoutes.js
@@ -1,8 +1,11 @@
 
-const { register } = require('../controller/usersController.js')
+const { register, login, setAvatar } = require('../controller/usersController.js')
 const { Router } = require('express');
 const router = Router();
 
 router.post("/register", register)
+router.post("/login", login)
+// 注意此处的id是动态的
+router.post("/setAvatar/:id", setAvatar)
 
 module.exports = router

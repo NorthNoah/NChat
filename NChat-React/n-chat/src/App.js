5ec88e4738e70218d639e3f8de90aff009fd559c
NorthNoah <1264918006@qq.com>
2023-05-29 20:25:11 +0800

    feat:错误边界处理

--- NChat-React/n-chat/src/App.js
+++ NChat-React/n-chat/src/App.js
@@ -1,23 +1,28 @@
 import socketIO from "socket.io-client"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ErrorFallback } from "./components/ErrorBoundary"
+import { ErrorBoundary } from "react-error-boundary"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
-import Chat from './pages/Chat'
-import SetAvatar from './pages/SetAvatar'
+import Chat from "./pages/Chat"
+import SetAvatar from "./pages/SetAvatar"
+
 // const socket = socketIO.connect("http://localhost:6000")
 
 function App() {
 	return (
-		<BrowserRouter>
-			<div className="App">
-				<Routes>
-					<Route path="/register" element={<Register />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/setAvatar" element={<SetAvatar />} />
-					<Route path="/" element={<Chat />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
+		<ErrorBoundary FallbackComponent={ErrorFallback}>
+			<BrowserRouter>
+				<div className="App">
+					<Routes>
+						<Route path="/register" element={<Register />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/setAvatar" element={<SetAvatar />} />
+						<Route path="/" element={<Chat />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</ErrorBoundary>
 	)
 }
 

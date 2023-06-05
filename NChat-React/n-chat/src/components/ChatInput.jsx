8c0b495bde6dc22240e709678d0bc40e2f94c06f
NorthNoah <1264918006@qq.com>
2023-06-05 11:05:09 +0800

    feat：聊天输入框

---
+++ NChat-React/n-chat/src/components/ChatInput.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+import React, { useEffect, useState } from "react"
+import Logo from "../assets/logo.svg"
+import { Avatar } from "antd"
+import Picker from "emoji-picker-react"
+export default function ChatInput() {
+	return <Container>
+    <div className="button-container">
+      <div className="emoji">
+
+      </div>
+    </div>
+  </Container>
+}
+const Container = styled.div`
+  
+
+`

9a4e45027d09618b041dd41eb738880dbe007d89
NorthNoah <1264918006@qq.com>
2023-06-05 18:39:56 +0800

    style:聊天框样式更新

--- NChat-React/n-chat/src/components/ChatContainer.jsx
+++ NChat-React/n-chat/src/components/ChatContainer.jsx
@@ -1,14 +1,12 @@
-import React,{useEffect} from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 import { Avatar } from "antd"
 import Logout from "./Logout"
-import { useNavigate } from "react-router-dom"
 import ChatInput from "./ChatInput"
+import Messages from "./Messages"
 
-export default function ChatContainer({ currentChat={} }) {
-  const handleSendMsg = async (msg) => {
-    
-  }
+export default function ChatContainer({ currentChat = {} }) {
+	const handleSendMsg = async (msg) => {}
 	return (
 		<div>
 			<Container>
@@ -24,36 +22,85 @@ export default function ChatContainer({ currentChat={} }) {
 							/>
 						</div>
 						<div className="username">
-              <h3>{currentChat.username}</h3>
-            </div>
+							<h3>{currentChat.username}</h3>
+						</div>
 					</div>
-          <Logout/>
+					<Logout />
 				</div>
-        <div className="chat-messages"></div>
-        <div className="chat-input"></div>
-        <ChatInput handleSendMsg={handleSendMsg}/>
+				<div className="chat-messages">
+					<Messages />
+				</div>
+				<ChatInput handleSendMsg={handleSendMsg} />
 			</Container>
 		</div>
 	)
 }
 const Container = styled.div`
-  padding-top: 1rem;
-  .chat-header {
+  height: 85vh;
+	display: grid;
+	grid-template-rows: 10% 80% 10%;
+	gap: 0.1rem;
+	/* overflow: hidden; */
+	@media screen and (min-width: 720px) and (max-width: 1080px) {
+		grid-template-rows: 15% 70% 15%;
+	}
+	.chat-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 2rem;
+		.user-details {
+			display: flex;
+			align-items: center;
+			gap: 1rem;
+			.avatar {
+			}
+			.username {
+				h3 {
+					color: white;
+				}
+			}
+		}
+	}
+  .chat-messages {
+    padding: 1rem 2rem;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 2rem;
-    .user-details {
+    flex-direction: column;
+    gap: 1rem;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .message {
       display: flex;
       align-items: center;
-      gap: 1rem;
-      .avatar {
-
-      }
-      .username {
-        h3 {
-          color: white;
+      .content {
+        max-width: 40%;
+        overflow-wrap: break-word;
+        padding: 1rem;
+        font-size: 1.1rem;
+        border-radius: 1rem;
+        color: #d1d1d1;
+        @media screen and (min-width: 720px) and (max-width: 1080px) {
+          max-width: 70%;
         }
+      }
+    }
+    .sended {
+      justify-content: flex-end;
+      .content {
+        background-color: #4f04ff21;
+      }
+    }
+    .recieved {
+      justify-content: flex-start;
+      .content {
+        background-color: #9900ff20;
       }
     }
   }
